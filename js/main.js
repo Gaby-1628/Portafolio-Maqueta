6f7544ca4952b06f69fd631fa596b8f27eed1821
Marisol Rodriguez <wvk_28@hotmail.com>
2023-06-01 15:23:38 -0300

    Portafolio

---
+++ js/main.js
@@ -0,0 +1,61 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.fa-html5', {
+    duration: 10000,
+    origin: 'rigth',
+    distance: '-100px'
+});
+
+sr.reveal('.fa-css3-alt', {
+    duration: 9000,
+    origin: 'rigth',
+    distance: '-100px'
+});
+
+sr.reveal('.fa-square-js', {
+    duration: 8000,
+    origin: 'rigth',
+    distance: '-100px'
+});
+
+sr.reveal('.fa-database', {
+    duration: 7000,
+    origin: 'rigth',
+    distance: '-100px'
+});
+
+sr.reveal('.fa-node', {
+    duration: 6000,
+    origin: 'rigth',
+    distance: '-100px'
+});
+
+sr.reveal('.fa-react', {
+    duration: 5000,
+    origin: 'rigth',
+    distance: '-100px'
+});
+
+sr.reveal('.fa-git-alt', {
+    duration: 4000,
+    origin: 'rigth',
+    distance: '-100px'
+});
+
+sr.reveal('.fa-github', {
+    duration: 3000,
+    origin: 'rigth',
+    distance: '-100px'
+});
+
+sr.reveal('.fa-figma', {
+    duration: 2000,
+    origin: 'rigth',
+    distance: '-100px'
+});
+
+sr.reveal('.fa-npm', {
+    duration: 1000,
+    origin: 'rigth',
+    distance: '-100px'
+});
